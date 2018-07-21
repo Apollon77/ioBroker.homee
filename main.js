e9dd378d495b8e8b6e9da7cd2c82a8b1266019c3
Ingo Fischer <github@fischer-ka.de>
2018-07-22 00:15:40 +0200

    fix rgb

--- main.js
+++ main.js
@@ -70,14 +70,14 @@ adapter.on('message', function (msg) {
 });
 
 function numberColorToHex(i) {
-    var bbggrr =  ("000000" + i.toString(16)).slice(-6);
-    var rrggbb = bbggrr.substr(4, 2) + bbggrr.substr(2, 2) + bbggrr.substr(0, 2);
+    var rrggbb =  ("000000" + i.toString(16)).slice(-6);
+    rrggbb = rrggbb.substr(0, 2) + rrggbb.substr(2, 2) + rrggbb.substr(4, 2);
     return rrggbb;
 }
 
 function hexToNumberColor(rrggbb) {
-    var bbggrr = rrggbb.substr(4, 2) + rrggbb.substr(2, 2) + rrggbb.substr(0, 2);
-    return parseInt(bbggrr, 16);
+    rrggbb = rrggbb.substr(0, 2) + rrggbb.substr(2, 2) + rrggbb.substr(4, 2);
+    return parseInt(rrggbb, 16);
 }
 
 adapter.on('stateChange', (id, state) => {
